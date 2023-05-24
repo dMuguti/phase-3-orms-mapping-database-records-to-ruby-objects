dc4bd869b76fea864eb4a6dfa6c933f4c4f2d2f5
dMuguti <omiahderek56@gmail.com>
2023-05-24 21:36:00 +0300

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,17 @@
+require 'sqlite3'
 class Song
-
   attr_accessor :name, :album, :id
-
   def initialize(name:, album:, id: nil)
     @id = id
     @name = name
     @album = album
   end
-
   def self.drop_table
     sql = <<-SQL
       DROP TABLE IF EXISTS songs
     SQL
-
     DB[:conn].execute(sql)
   end
-
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
@@ -24,29 +20,37 @@ class Song
         album TEXT
       )
     SQL
-
     DB[:conn].execute(sql)
   end
-
   def save
     sql = <<-SQL
       INSERT INTO songs (name, album)
       VALUES (?, ?)
     SQL
-
-    # insert the song
     DB[:conn].execute(sql, self.name, self.album)
-
-    # get the song ID from the database and save it to the Ruby instance
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
-    # return the Ruby instance
     self
   end
-
   def self.create(name:, album:)
     song = Song.new(name: name, album: album)
     song.save
   end
-
+  def self.new_from_db(row)
+    id, name, album = row
+    Song.new(id: id, name: name, album: album)
+  end
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM songs
+    SQL
+    results = DB[:conn].execute(sql)
+    results.map { |row| self.new_from_db(row) }
+  end
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM songs WHERE name = ?
+    SQL
+    result = DB[:conn].execute(sql, name).first
+    self.new_from_db(result) if result
+  end
 end
